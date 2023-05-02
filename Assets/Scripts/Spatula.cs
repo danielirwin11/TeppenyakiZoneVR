08817196a940568a58f50fd8339b8399f482865c
danielirwin11 <daniel.irwin18@outlook.com>
2023-05-03 05:40:10 +1000

    project upload again

---
+++ Assets/Scripts/Spatula.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spatula : MonoBehaviour
+{
+//    Spatula Script Goals
+//Allow user to pickup spatula from handle AND have it always rotate correctly
+//If spatula is dropped, it teleports back to its origin.If spatula is dropped near origin it moves to origin
+//If spatula scrapes against grill, make a sound
+//If spatula comes into contact with other tools, or just bangs against grill, make a sound
+//If spatula is inserted between food and grill, food slides onto top side of spatula
+//Food is then flipped once spatula itself is flipped
+}
